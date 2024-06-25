2a9199387349e8f7f116437de354a88a7d6f44df
mariinkys <kysgeneral.nrgua@slmail.me>
2024-06-25 15:56:20 +0200

    changed default app resolution

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -12,7 +12,7 @@ pub fn get_app_settings() -> Settings {
     let mut settings = Settings::default();
 
     settings = settings.size_limits(Limits::NONE.min_width(400.0).min_height(180.0));
-    settings = settings.size(Size::new(800.0, 800.0));
+    settings = settings.size(Size::new(1200.0, 800.0));
     settings = settings.debug(false);
     settings
 }
